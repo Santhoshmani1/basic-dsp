b0e4326f5f7cf9a0ab1eb8f600c608508cb8dcf4
Santhosh Mani <pitakasanthosh@gmail.com>
2025-12-03 20:31:33 +0530

    Refactor image processing operations in basicoperations.m

--- exp11-basic-operations/basicoperations.m
+++ exp11-basic-operations/basicoperations.m
@@ -1,35 +1,19 @@
 clc;
+close all;
 clear all;
 
-I = imread('/MATLAB Drive/image.jpg');
-I1 = rgb2gray(I);
-subplot(4,2,1);
-imshow(I);
-title('Original Image');
+I = imread('phoenix.jpg');
 
-subplot(4,2,2);
-imshow(I1);
-title('Grayscale Image');
+rgb_pho = rgb2gray(I);
+ro_ph = imrotate(I, 45);
+hf_ph = flip(I, 1);
+vf_ph = flip(I, 2);
+rs_ph = imresize(I, [256 256]); % resized
 
-I2=imread('/MATLAB Drive/image.jpg')
-I3=rgb2gray(I2)
-subplot(4,2,3)
-imshow(I2)
-
-K=imrotate(I2,90)
-imshow(K)
-title('Rotated Image 90DEG');
-K2 = imresize(I, [100 100]); 
-subplot(4,2,4);
-imshow(K2);
-title('Resized image');
-
-K3 = flip(I, 2);
-subplot(4,2,5);
-imshow(K3);
-title('Horizontally Flipped Image');
-
-K4 = flip(I, 1);
-subplot(4,2,6);
-imshow(K4);
-title('Vertically Flipped Image');
+figure;
+subplot(2,3,1); imshow(I); title('Original');
+subplot(2,3,2); imshow(rgb_pho); title('Grayscale');
+subplot(2,3,3); imshow(ro_ph); title('Rotated 45°');
+subplot(2,3,4); imshow(hf_ph); title('Horizontal Flip');
+subplot(2,3,5); imshow(vf_ph); title('Vertical Flip');
+subplot(2,3,6); imshow(rs_ph); title('Resized 256×256');
